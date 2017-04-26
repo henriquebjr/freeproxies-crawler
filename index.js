d857f4646426206f333586b48c9ce73ca90188a4
Henrique <henriquebjr@gmail.com>
2017-04-25 22:54:03 -0300

    it works

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 'use strict';
 
+let express = require('express');
+let app = express();
 let cheerio = require('cheerio');
 let spawn = require('child_process').spawn;
 let url = 'http://www.freeproxylists.net/?c=&pt=&pr=&a%5B%5D=0&a%5B%5D=1&a%5B%5D=2&u=90';
-let phantom = spawn('phantomjs', ['phantom.js', url]);
+
 let finalData = [];
 
-phantom.stdout.on('data', data => {
-  finalData.push(data);
-});
+app.get('', function(req, res){
+  let phantom = spawn('phantomjs', ['phantom.js', url]);
 
-phantom.on('close', () => {
-  finalData = Buffer.concat(finalData);
-  finalData = JSON.parse(finalData);
-
-  let $ = cheerio.load(finalData.html);
-  let $tr = $('.DataGrid tr:not(:first-child)');
-
-  $tr.each((index, item) => {
-    let $actualTr = $(item);
-    let proxy = $actualTr.find('a').text();
-    let port = $actualTr.find('td:nth-child(2)').text();
-    let country = $actualTr.find('td:nth-child(5)').text();
-    if(proxy && port)
-      console.log(`${proxy}:${port} - ${country}`);
+  phantom.stdout.on('data', data => {
+    finalData.push(data);
   });
+
+  phantom.on('close', () => {
+    finalData = Buffer.concat(finalData);
+    finalData = JSON.parse(finalData);
+
+    let $ = cheerio.load(finalData.html);
+    let $tr = $('.DataGrid tr:not(:first-child)');
+    let proxies = [];
+    let number = 0;
+
+    $tr.each((index, item) => {
+      let $actualTr = $(item);
+      let proxy = $actualTr.find('a').text();
+      let port = $actualTr.find('td:nth-child(2)').text();
+      let country = $actualTr.find('td:nth-child(5)').text();
+      if (proxy && port) {
+        console.log(`${proxy}:${port} - ${country}`);
+        proxies[number++] = { proxy: proxy, port: port, country: country };
+      }
+    });
+
+    res.end( JSON.stringify(proxies) );
+  });
+
+  //res.end( 'teste' );
 });
 
+let server = app.listen(3000, function() {
+  console.log("Listening at port 3000");
+})
